18041bf28cd9b0deaf6a7fdf6998b4157f697c17
Your Name <you@example.com>
2024-07-28 20:16:53 +0530

    Adding movie and theater

--- book-my-show/src/main/java/com/acciojob/book_my_show/Repositories/MovieRepository.java
+++ book-my-show/src/main/java/com/acciojob/book_my_show/Repositories/MovieRepository.java
@@ -4,13 +4,7 @@ import com.acciojob.book_my_show.Enums.Language;
 import com.acciojob.book_my_show.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MovieRepository extends JpaRepository<Movie,Integer> {
 
-Movie findMovieByMovieName(String movieName);
-    List<Movie> findAllByLanguage(Language language);
-
-
-
+    Movie findMovieByMovieName(String movieName);
 }
